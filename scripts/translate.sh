7e0b8fb1b0e8d5b4762bbb47c4a300b077c3a5b2
alibrahimzada <ali.reza.ibrahimzada@ibm.com>
2023-08-01 20:29:33 -0400

    update bash script

--- scripts/translate.sh
+++ scripts/translate.sh
@@ -4,7 +4,7 @@ export PYTHONIOENCODING=utf-8;
 
 function prompt() {
     echo;
-    echo "Syntax: bash scripts/translate.sh MODEL DATASET SRC_LANG TRG_LANG K P TEMPERATURE";
+    echo "Syntax: bash scripts/translate.sh MODEL DATASET SRC_LANG TRG_LANG K P TEMPERATURE GPU_ID";
     echo "MODEL: name of the model to use";
     echo "DATASET: name of the dataset to use";
     echo "SRC_LANG: source language";
@@ -12,6 +12,7 @@ function prompt() {
     echo "K: top-k sampling";
     echo "P: top-p sampling";
     echo "TEMPERATURE: temperature for sampling";
+    echo "GPU_ID: GPU to use";
     exit;
 }
 
@@ -22,7 +23,7 @@ while getopts ":h" option; do
     esac
 done
 
-if [[ $# < 7 ]]; then
+if [[ $# < 8 ]]; then
   prompt;
 fi
 
@@ -33,5 +34,12 @@ TRG_LANG=$4;
 K=$5;
 P=$6;
 TEMPERATURE=$7;
+GPU_ID=$8;
 
-python3 src/translation/translate.py --model $MODEL --dataset $DATASET --source_lang $SRC_LANG --target_lang $TRG_LANG --k $K --p $P --temperature $TEMPERATURE;
+if [[ $MODEL == "GPT-4" ]]; then
+  python3 src/translation/translate_gpt4.py --dataset $DATASET --source_lang $SRC_LANG --target_lang $TRG_LANG --k $K --p $P --temperature $TEMPERATURE;
+elif [[ $MODEL == "StarCoder" || $MODEL == "CodeGen" || $MODEL == "CodeGeeX" || $MODEL == "LLaMa" || $MODEL == "TB-Airoboros" || $MODEL == "TB-Vicuna" ]]; then
+  python3 src/translation/translate_open_source.py --model $MODEL --dataset $DATASET --source_lang $SRC_LANG --target_lang $TRG_LANG --k $K --p $P --temperature $TEMPERATURE --gpu_id $GPU_ID;
+else
+  echo "Model not supported";
+fi
